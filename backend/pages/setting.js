b89e0f06e1983cdc916e328571ff44721882ac11
Abdellah Baqba <abdobaq777@gmail.com>
2026-02-12 18:57:09 +0100

    change info

--- backend/pages/setting.js
+++ backend/pages/setting.js
@@ -25,7 +25,7 @@ export default function Setting() {
                 {/* title dashboard */}
                 <div className="titledashboard flex flex-sb">
                     <div data-aos="fade-right">
-                        <h2>Admin <span>Settings</span></h2>
+                        <h2>Admin Abdellah Baqba<span>Settings</span></h2>
                         <h3>ADMIN PANEL</h3>
                     </div>
                     <div className="breadcrumb" data-aos="fade-left">
@@ -39,14 +39,14 @@ export default function Setting() {
                         <div className="w-100" data-aos="fade-up">
                             <div className='flex flex-sb flex-left mt-2'>
                                 <h2>My Profile:</h2>
-                                <h3>Vbm Coder <br /> web developer</h3>
+                                <h3>Abdellah Baqba Coder <br /> web developer</h3>
                             </div>
                             <div className="flex flex-sb mt-2">
                                 <h3>Phone:</h3>
-                                <input type="text" defaultValue="+91-0123456789" />
+                                <input type="text" defaultValue="+212 676367706" />
                             </div>
                             <div className="mt-2">
-                                <input type="email" defaultValue="Youremailaddress@gmail.com" />
+                                <input type="email" defaultValue="abdobaq777@gmail.com" />
                             </div>
                             <div className="flex flex-center w-100 mt-2">
                                 <button>Save</button>
