89ecba2e24c92bd61e583a456caf370a70fe8f09
Abdellah Baqba <abdobaq777@gmail.com>
2026-02-12 21:00:39 +0100

    modify span

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -205,7 +205,7 @@ export default function blogs() {
 
                                                 Bona autem corporis huic sunt, quod posterius posui, similiora. Quod cum ita sit,
                                                 perspicuum est omnis rectas res atque laudabilis eo referri, ut</p>
-                                            <h4 className="flex"><img src="img/coder.jpg" alt="" /><span>by vbm coder</span></h4>
+                                            <h4 className="flex"><img src="img/coder.jpg" alt="" /><span>by Abdellah baqba coder</span></h4>
                                         </div>
                                     </div>
                                 })}
